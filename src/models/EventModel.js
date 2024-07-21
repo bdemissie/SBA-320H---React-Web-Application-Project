43b20d630a60ebcf3b8fe40f8f0b9db4e7e5f43c
bdemissie <bemnet.hdemissie@gmail.com>
2024-07-21 00:12:07 -0400

    eventModel and eventData added

---
+++ src/models/EventModel.js
@@ -0,0 +1,18 @@
+class EventModel {
+
+    constructor(id, eventName, description, date, location, organizer, image, catergory)  {
+
+        id = this.id;
+        eventName = this.eventName;
+        description = this.description;
+        date = this.date;
+        location = this.location;
+        organizer = this.organizer;
+        image = this.image;
+        catergory = this.description
+
+    }
+
+}
+
+export default EventModel;
